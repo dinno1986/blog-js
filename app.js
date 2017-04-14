704d38b28149d6b6f6214b8b2512a0bb5d2d652b
dinno1986 <dimitri.castell@hotmail.fr>
2017-04-14 16:03:31 +0200

    quand je clique sur le 1er titre cela m'envoi sur l'article correspondant etc

--- app.js
+++ app.js
@@ -38,6 +38,16 @@ $.ajax({
 		}
 });
 
+$("#clearbutt").click(function(){
+	$.ajax({  
+		url:'http://192.168.1.50/json-db',
+		data: {
+			task: 'delete',
+			key: 'dimblog'
+		}
+	});	
+});
+
 
 
 function afficher( listeArticles ){
@@ -45,11 +55,21 @@ function afficher( listeArticles ){
 	for ( var i=0 ; i<listeArticles.length ; i++ ){
 		var article = listeArticles[i];
 
-		console.log( article );
+		//console.log( article );
 
-		$("#intitule").append('<li role="presentation"><a href="#">'+(article.tit)+'</a></li>');
+		$("#intitule").append('<li  role="presentation"><a class="clickTitre" value="'+i+'" href="#">'+(article.tit)+'</a></li>');
 		$("#article").append( article.tex );
+		$("#article").html("");
 
 	}
+	$(".clickTitre").click(function(){
+		//console.log("clickTitre");
+		var indArt = $(this).attr('value');
+		//console.log(indArt);
+		$("#article").html(listeArticles[indArt].tex);
+
+	});
+
 };
+
 
