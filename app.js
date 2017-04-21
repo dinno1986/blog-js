4431b45bc7487675ad3c0af08ac522f3ef06f6b8
dinno1986 <dimitri.castell@hotmail.fr>
2017-04-21 10:56:00 +0200

    Ajout du bouton supprimer et je supprime les titres sélectionnés

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-//var blog=[];
+
 
 
 
@@ -7,11 +7,9 @@ $("#envoi").click(function(){
 	var texte = $("#text").val();
 	var uneDate = new Date();
 	var recup = {"tit":titre, "tex":texte, "dat":uneDate};
-//	blog.push(recup);
-//	console.log(blog);
 
-	var app={name:"dim"};
-	
+
+	//envoyer	
 	$.ajax({
 		url: 'http://192.168.1.50/json-db',
 		data : {
@@ -23,9 +21,8 @@ $("#envoi").click(function(){
 });
 
 
-
-
-$.ajax({
+	//appeler la fonction
+	$.ajax({
 		url:'http://192.168.1.50/json-db',
 		data: {
 			task: 'get',
@@ -33,58 +30,77 @@ $.ajax({
 		},
 		success : function(data){
 			var mesArticles = JSON.parse(data);
-
 			afficher( JSON.parse(data));
+			list ( JSON.parse(data))
+				console.log(mesArticles);//pour trouver l'ID des objets
+			}
+		});
+
+
+	
+
+
+	function afficher( listeArticles ){
+
+		for ( var i=0 ; i<listeArticles.length ; i++ ){
+			var article = listeArticles[i];
+
+			//console.log( article );
+			$("#intitule").append('<li role="presentation"><a class="clickTitre" value="'+i+'" href="#">'+(article.tit)+'</a></li>');
+			$("#article").append( article.tex );
+			$("#horloge").html(article.dat);
+			//console.log(article.dat);
+			$("#article").html("");
+
 		}
-});
+		$(".clickTitre").click(function(){
+			//console.log("clickTitre");
+			var indArt = $(this).attr('value');
+			//console.log(indArt);
+			$("#article").html(listeArticles[indArt].tex);
+		});
+	};
 
-$("#clearbutt").click(function(){
-	$.ajax({  
-		url:'http://192.168.1.50/json-db',
-		data: {
-			task: 'delete',
-			key: 'dimblog'
+
+	$("#text").on('keyup',function(){
+
+		var convertir = new showdown.Converter();
+		text      = $("#text").val();
+		var html      = convertir.makeHtml(text);
+		$("#text2").html(html);
+
+	});
+
+
+
+	function list( mesArticles ) {
+
+		for ( var i=0 ; i<mesArticles.length ; i++ ) {
+			var article = mesArticles[i];
+			console.log( article );
+
+			$(".icone").append('<li role="presentation"><a class="clickTitreAdmin" value="'+i+'" href="#" >'+article.tit+'</a></li>');
+			$(".icone").append('<button class="suprim" data-id="' + article._id + '">Suppr</button>');
 		}
-	});	
-});
+	
 
+		 $(".suprim").click(function(){
+			var cur_Id = $(this).data('id');
+			console.log(cur_Id)
+	 		$.ajax({  
+				url:'http://192.168.1.50/json-db',
+					data: {
+						task: 'delete',
+						_id: cur_Id,
 
+	 				}
 
-function afficher( listeArticles ){
+			});
 
-	for ( var i=0 ; i<listeArticles.length ; i++ ){
-		var article = listeArticles[i];
+		});
 
-		//console.log( article );
-
-		$("#intitule").append('<li  role="presentation"><a class="clickTitre" value="'+i+'" href="#">'+(article.tit)+'</a></li>');
-		$("#article").append( article.tex );
-		//console.log(article.dat);
-		$("#horloge").html(article.dat);
-		$("#article").html("");
 
 	}
-	$(".clickTitre").click(function(){
-		//console.log("clickTitre");
-		var indArt = $(this).attr('value');
-		//console.log(indArt);
-		$("#article").html(listeArticles[indArt].tex);
-	});
-};
-
-
-$("#text").on('keyup',function(){
-
-var convertir = new showdown.Converter();
-    text      = $("#text").val();
-    var html      = convertir.makeHtml(text);
-$("#text2").html(html);
-
-});
-
-
-
-
 
 
 
