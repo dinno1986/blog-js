bbdfa737176f84e0229463b382e0f2f3f4772c62
dinno1986 <dimitri.castell@hotmail.fr>
2017-05-16 10:25:30 +0200

    utilisation de node fs pour enregistrer et sauvegarder

--- server.js
+++ server.js
@@ -3,13 +3,14 @@
 var express = require('express');
 var app = express();
 var bodyparser = require('body-parser');
+var fs = require('fs');
 
 app.listen(3000, function(){
 	console.log('server ok');
 });
 
 var articles =[];
-
+charger();
 //middleware
 
 app.use(bodyparser.urlencoded({extended:false}));
@@ -20,7 +21,22 @@ app.use(express.static(__dirname + '/'));
 app.set('views', './views');
 app.set('view engine', 'jade');
 
-//routes
+//node fs
+//afficher les elements sauvegardes
+function charger () {
+	fs.readFile('dim.json', function(err, data){
+  		if (err) throw err;
+  		articles=JSON.parse(data);
+	});
+};
+
+function sauvegarde (){
+	fs.writeFile('dim.json', JSON.stringify(articles), function(err){
+ 		 if (err) throw err;
+  		console.log('The file has been saved!');
+	});
+};
+
 
 app.get('/', function(req, res){
 	res.render('admin');
@@ -29,15 +45,22 @@ app.get('/', function(req, res){
 app.get('/index', function(req,res){
 	res.render('index');
 })
-
+//route pour enregistrer
 app.post('/Dimblog', function (req, res) {
 	var save = JSON.parse(req.body.value);
 	articles.push (save);
-   console.log(articles);
+	sauvegarde();
+	res.send('ok');
+   //console.log(articles);
+   		
 });
-
+//route pour afficher
 app.get('/Koko', function (req, res){
 	res.send(JSON.stringify(articles));
 });
+//route pour effacer
+app.get('/Delete', function(req, res){
+
+})
 
 
